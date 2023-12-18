3d5a992d8afc8ffcb8a0a862ae84082bc0c6a7a6
manojprabhakar1 <manojsairam0@gmail.com>
2023-12-18 14:46:15 +0530

    Code3

---
+++ Code3/Calci/main.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "calculator.h"
+
+int main() {
+    Calculator calc;
+
+    double num1, num2;
+    char op;
+        
+    
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter operator (+, -, *, /): ";
+    
+    std::cin >> op;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    double result;
+    switch (op) {
+        case '+':
+            result = calc.add(num1, num2);
+            break;
+        case '-':
+            result = calc.subtract(num1, num2);
+            break;
+        case '*':
+            result = calc.multiply(num1, num2);
+            break;
+        case '/':
+            result = calc.divide(num1, num2);
+            break;
+        
+        
+        default:
+            std::cout << "Invalid operator!";
+            return 1;
+    
+    }
+
+    std::cout << "Result: " << result << std::endl;
+    
+    return 0;
+}
